bdb01ead06ce48580871b6fd3c3a13216967be62
Fredrik Enes <594754@stud.hvl.no>
2024-08-26 13:51:23 +0200

    Just a test program

---
+++ expass1/src/main/java/no/hvl/dat250/expass1/controller/FrontpageController.java
@@ -0,0 +1,16 @@
+package no.hvl.dat250.expass1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FrontpageController {
+
+    @GetMapping
+    public String showHelloWorld() {
+        return "Hello World!";
+    }
+}
